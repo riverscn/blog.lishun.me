e8e3a4a0a27324c309c343ed6d8c465df4cb3e88
Shun Li <riverscn@gmail.com>
2018-08-29 15:52:28 +0800

    update deploy method

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t hugo_theme_pickles --cleanDestinationDir # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo_theme_pickles # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add .
 
